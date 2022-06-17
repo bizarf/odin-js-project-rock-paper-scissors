2da8c4076f665e13f72b242cc745671d135c4f88
bizarf <snesmaster40@googlemail.com>
2022-06-17 18:42:23 +0100

    Added buttons, and recoded some of the game.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,3 +1,7 @@
+const winResult = "You win!";
+const loseResult = "You lose!";
+const drawResult = "It's a draw!";
+
 // Determines the computer's choice
 function computerPlay() {
     const computerPlayOptions = [
@@ -13,56 +17,57 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     // player chooses rock
     if (playerSelection === "rock" && computerSelection === "Rock") {
-        return "It's a draw!"
+        return drawResult
     } else if (playerSelection === "rock" && computerSelection === "Paper") {
         computerScore++;
-        return "You lose! Paper beats Rock"
+        return loseResult
     } else if (playerSelection === "rock" && computerSelection === "Scissors") {
         playerScore++;
-        return "You win! Rock beats Scissors"
+        return winResult
 
         // player choose paper
     } else if (playerSelection === "paper" && computerSelection === "Rock") {
         playerScore++;
-        return "You win! Paper beats Rock"
+        return winResult
     } else if (playerSelection === "paper" && computerSelection === "Paper") {
-        return "It's a draw!"
+        return drawResult
     } else if (playerSelection === "paper" && computerSelection === "Scissors") {
         computerScore++;
-        return "You lose! Scissors beats Paper"
+        return loseResult
 
         // player chooses scissors
     } else if (playerSelection === "scissors" && computerSelection === "Rock") {
         computerScore++;
-        return "You lose! Rock beats Scissors"
+        return loseResult
     } else if (playerSelection === "scissors" && computerSelection === "Paper") {
         playerScore++;
-        return "You win! Scissors beats Paper"
+        return winResult
     } else if (playerSelection === "scissors" && computerSelection === "Scissors") {
-        return "It's a draw!"
+        return drawResult
     } else {
         return "Please choose from either Rock, Paper, or Scissors"
     }
 
 }
 
-let playerPlay = prompt("Rock, Paper, or Scissors?").toLowerCase();
-const playerSelection = playerPlay;
+// let playerPlay = prompt("Rock, Paper, or Scissors?").toLowerCase();
+// const playerSelection = playerPlay;
 
 let playerScore = 0;
 let computerScore = 0;
 
 function game() {
 
-    for (let i = 1; i < 6; i++) {
-        const computerSelection = computerPlay(); // This allows the computer to make a different choice for each round. 
-        console.log("Round " + i);
-        console.log(playRound(playerSelection, computerSelection));
+    for (let i = 0; i < 5; i++) {
+        // const computerSelection = computerPlay(); // This allows the computer to make a different choice for each round. 
+        console.log(`Round ${i + 1}`);
+        // console.log(playRound(playerSelection, computerSelection));
+        console.log(playRound(playerSelection, computerPlay()));
     }
     console.log("Player's score " + playerScore);
     console.log("Computer's score " + computerScore);
     if (playerScore > computerScore) {
-        console.log("Player wins!") 
+        console.log("Player wins!")
     } else if (computerScore > playerScore) {
         console.log("Computer wins!")
     } else if (playerScore === computerScore) {
@@ -70,4 +75,24 @@ function game() {
     }
 }
 
-game(); // Starts the game
+// game(); // Starts the game
+const roundResult = document.querySelector("#roundResult")
+
+const playerRock = document.querySelector('#rockBtn');
+playerRock.addEventListener('click', () => {
+    // console.log(playRound("rock", computerPlay()))
+
+    roundResult.textContent = (playRound("rock", computerPlay()))
+})
+
+const playerPaper = document.querySelector('#paperBtn');
+playerPaper.addEventListener('click', () => {
+    // console.log(playRound("paper", computerPlay()))
+    roundResult.textContent = (playRound("paper", computerPlay()))
+})
+
+const playerScissors = document.querySelector('#scissorsBtn');
+playerScissors.addEventListener('click', () => {
+    // console.log(playRound("scissors", computerPlay()))
+    roundResult.textContent = (playRound("scissors", computerPlay()))
+})
